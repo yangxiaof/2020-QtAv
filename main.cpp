e89c4ca1f9d1933af96b3cabcb62487fbdda4a25
Stella <gongbenxiaoli@163.com>
2020-04-08 22:49:50 +0800

    第一个程序

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿#include "mainwindow.h"
+#include <QDebug>
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    qDebug() << QString::fromLocal8Bit("第一个上传git的程序");
+    w.show();
+    return a.exec();
+}
